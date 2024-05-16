8609e2be3496de4c549687face4aed935c18d895
yunyeobin <sky_line22@naver.com>
2024-05-16 18:36:53 +0900

    #6 feat: 이미지 처리전 커밋

--- src/main/java/com/ssg/dsilbackend/repository/ReserveRepository.java
+++ src/main/java/com/ssg/dsilbackend/repository/ReserveRepository.java
@@ -2,9 +2,18 @@ package com.ssg.dsilbackend.repository;
 
 import com.ssg.dsilbackend.domain.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ReserveRepository extends JpaRepository<Reservation, Long> {
     List<Reservation> findByRestaurantId(Long restaurantId);
-
+    @Query("SELECT r FROM Reservation r WHERE r.restaurant.id = :restaurantId AND r.reservationDate BETWEEN :startDate AND :endDate")
+    List<Reservation> findByRestaurantIdAndReservationDateBetween(
+            @Param("restaurantId") Long restaurantId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate
+    );
 }
